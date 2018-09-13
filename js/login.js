1a42823714109d5d477c5c428810156ac305d529
Markus Schelbert <markus.schelbert@linurix.ch>
2018-09-13 18:09:46 +0200

    Style und Login angepasst

--- js/login.js
+++ js/login.js
@@ -7,18 +7,31 @@ $('section#login form button').click(function() {
     localStorage.setItem('benutzername', benutzername);
     localStorage.setItem('useragent', navigator.userAgent);
     
+    $('section#login form button').html('wird überprüft')
+    
     $.ajax({
         url: server,
         method: 'post',
         dataType: 'json',
         data: {benutzername: benutzername, passwort:passwort, useragent:navigator.userAgent}
     }).done(function(data) {
-        console.log('Logindaten werden gespeichert ...')
         localStorage.setItem('section', data.section);
         localStorage.setItem('fingerprint', data.fingerprint);
         $('section').hide();
         $('section#' + data.section).show();
+        $('section#login form button').addClass('error').html(data.alert);
+        setTimeout(function(){
+            $('section#login form button').removeClass('error').html('prüfen');
+        }, 3000);
+        console.log('Logindaten werden gespeichert ...')
         console.log(data.alert)        
+    }).fail(function() {
+        console.log('Logindaten konnten nicht geprüft werden');
+        
+        alert('Hallo');
+        $('section#login form button')
+            .addClass('error').html('Logindaten konnten nicht geprüft werden')
+            .delay(2000).html('überprüfen').removeClass('error');
     });
     
     return false;
